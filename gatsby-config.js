b6bfc59b42fdff9b908082ce30f1265ae933c8e3
Kayot88 <kayot88@gmail.com>
2019-04-13 18:10:10 +0300

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config()
+}
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gromov Roman\`s blog ` ,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -27,8 +30,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
   ],
 }
---
+++ src/components/Article.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+const Article = ({
+  content: {
+    title,
+    text: { text },
+    banner: {
+      file: { url },
+    },
+    publishedAt,
+  },
+}) => (
+  <React.Fragment>
+    <div>
+      <h2>title</h2>
+      <img src={url} alt={title} />
+      <p>{text}</p>
+      <h5>{publishedAt}</h5>
+    </div>
+  </React.Fragment>
+)
+
+export default Article
